132a3f4d21e751ee37d8b83c67d4d8bc9a1c4300
Vitali Griunvald <vgriunvald@gmail.com>
2023-08-19 11:55:12 +0300

    Use AppError class in a AuthController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,23 +1,28 @@
 import AuthRepository from '../repositories/AuthRepository.js';
+import AppError from '../middleware/error/AppError.js';
 
 class AuthController {
     constructor(AuthRepository) {
         this.authRepository = AuthRepository;
     }
 
-    async register(req, res) {
+    async register(req, res, next) {
         const { email, fullname, username, password } = req.body;
         try {
         const token = await this.authRepository.createUser(email, fullname, username, password);
         res.cookie('access_token', token, {httpOnly: true});
         res.status(201).json({ message: 'User was created!' });
         } catch (err) {
+            if(err.message === 'Username already taken!') {
+            next(new AppError(err.message, 400));
+            } else {
             console.error(err);
-            res.status(500).json({ error: 'Internal server error' });
+            next(new AppError('Internal server error', 500));
+            }
         }
     }
      
-async login(req, res) {
+async login(req, res, next) {
        const {input, password} = req.body;
        try {
            const token = await this.authRepository.login(input, password)
@@ -25,17 +30,17 @@ async login(req, res) {
            res.status(200).json({ message: 'Cookie set!' });
        }catch(err){
            console.error(err);
-           res.status(500).json({ error: 'Internal server error' });
+           next(new AppError('Internal server error', 500));
        } 
     }
 
-    logout(req, res){
+    logout(req, res, next){
         try{
            res.cookie('access_token', '', {httpOnly: true, expires: new Date(0)});
            res.status(200).json({ message: 'Logout successfully!' });
         }catch(err){
            console.error(err);
-           res.status(500).json({ error: 'Internal server error' });
+           next(new AppError('Internal server error', 500));
         }
     }
     
