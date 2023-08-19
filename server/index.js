77a98852df881af373309a4c44ba785839eed63e
Vitali Griunvald <vgriunvald@gmail.com>
2023-08-19 09:25:33 +0300

    Add custom error class and middleware

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import db from './db.js';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/authRoute.js';
+import errorHandler from './middleware/error/errorHandler.js';
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
+app.use(errorHandler);
 
 app.listen(3003, () => {
     console.log('Listening on a port 3003');
