3fa430741b6af0cd699ed5e634e98ca9eb70faf1
Vitali Griunvald <vgriunvald@gmail.com>
2023-08-31 05:58:47 +0300

    Check current trip status and based on that allow/disallow to create new trip

--- server/controllers/TripController.js
+++ server/controllers/TripController.js
@@ -9,6 +9,7 @@ class TripController {
        const {userId, title} = req.body;
         try {
            const trip = await this.tripRepository.createTrip(userId, title);
+            if(trip === 'open') return res.status(400).json({message: 'Please, close your previous trip!'})
            res.status(201).json({message: 'Trip was created!'})
         } catch(err){
             if (title.trim().length === 0) {
@@ -17,7 +18,7 @@ class TripController {
                next(new AppError('Trip title aready exists for this user', 400));
             } else if (err.message.includes('value too long for type character varying(100)')){
                next(new AppError('Trip title too long', 400));
-            } else {
+            } else { 
                console.error(err);
                next(new AppError('Internal server error', 500));
             }
--- server/repositories/TripRepository.js
+++ server/repositories/TripRepository.js
@@ -5,9 +5,27 @@ class TripRepository {
         this.pool = dbPool;
     }
     async createTrip(userId, title){
+        const tripStatus = await this.checkCurrentTripStatus(userId);
+        console.log(tripStatus);
+        if (tripStatus === 'open') return 'open';
         const client = await this.pool.connect();
         const insertQuery = `INSERT INTO trips (user_id, title) VALUES ($1, $2)`;
         const trip = await client.query(insertQuery, [userId, title]);
+    }
+
+    async checkCurrentTripStatus(userId){
+        const client = await this.pool.connect();
+        try{
+            const searchQuery = `SELECT status FROM trips WHERE user_id = $1`;
+            const searchResult = await client.query(searchQuery, [userId]);
+            return searchResult.rows[0].status;
+            } catch(err){
+                console.error(err);
+                throw err;
+            } finally {
+                client.release();
+            }
+
     }
 }
 
