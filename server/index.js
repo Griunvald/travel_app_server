4506c91dca5a43f0c39bab5c5705278d5349b13c
Vitali Griunvald <vgriunvald@gmail.com>
2023-09-30 17:35:58 +0300

    Setup cors to allow store cookies

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',  
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true 
+}));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/trip', tripRouter);
 app.use('/api/v1/record', recordRouter);
