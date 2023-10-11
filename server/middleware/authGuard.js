f73af73d86f0d1f51219c5065efdfb7e2eedbb89
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-11 20:16:33 +0300

    Refactoring authGuard middleware - access cookie instead headers

--- server/middleware/authGuard.js
+++ server/middleware/authGuard.js
@@ -2,11 +2,12 @@ import AuthRepository from '../repositories/AuthRepository.js';
 import AppError from '../middleware/error/AppError.js';
 
 const authGuard = async (req, res, next) => {
-    if(!req.headers.access_token){
+    const token = req.cookies.access_token;
+    if(!token){
         return next(new AppError('Unauthorized', 401));
     }
 
-    const userId = await AuthRepository.getUserIdFromToken(req.headers.access_token);
+    const userId = await AuthRepository.getUserIdFromToken(token);
     console.log(userId);
 
     next();
