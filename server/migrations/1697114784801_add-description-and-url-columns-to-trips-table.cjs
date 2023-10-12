938ce95175fa27986fa709cb45d7610868bfdb89
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-13 00:48:45 +0300

    Bug fix in migration file

--- server/migrations/1697114784801_add-description-and-url-columns-to-trips-table.cjs
+++ server/migrations/1697114784801_add-description-and-url-columns-to-trips-table.cjs
@@ -2,19 +2,22 @@ exports.shorthands = undefined;
 
 exports.up = pgm => {
     pgm.sql(`
-    ALTER TABLE trips ADD COLUMN description VARCHAR(300);
-    ALTER TABLE trips ALTER COLUMN description SET NOT NULL;
-    ALTER TABLE trips ALTER COLUMN description DROP NOT NULL;
-    ALTER TABLE trips DROP COLUMN description;
+        ALTER TABLE trips ADD COLUMN description VARCHAR(300);
+        ALTER TABLE trips ALTER COLUMN description SET NOT NULL;
+
+        ALTER TABLE trips ADD COLUMN url VARCHAR(300);
+        ALTER TABLE trips ALTER COLUMN url SET NOT NULL;
     `);
 };
 
 exports.down = pgm => {
     pgm.sql(`
-    ALTER TABLE trips ADD COLUMN url VARCHAR(100);
-    ALTER TABLE trips ALTER COLUMN url SET NOT NULL;
-    ALTER TABLE trips ALTER COLUMN url DROP NOT NULL;
-    ALTER TABLE trips DROP COLUMN url;
+        
+        ALTER TABLE trips ALTER COLUMN description DROP NOT NULL;
+        ALTER TABLE trips DROP COLUMN description;
+
+        ALTER TABLE trips ALTER COLUMN url DROP NOT NULL;
+        ALTER TABLE trips DROP COLUMN url;
     `);
 };
 
