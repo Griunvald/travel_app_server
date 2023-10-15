f12d0b992d692db12a74a28eb8fd82717c929922
Vitali Griunvald <vgriunvald@gmail.com>
2023-10-15 18:47:13 +0300

    Add migration - add column username to trips teble

---
+++ server/migrations/1697384415530_alter-table-trips-add-column-username.cjs
@@ -0,0 +1,17 @@
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.sql(`
+        ALTER TABLE trips ADD COLUMN username VARCHAR(50);
+        ALTER TABLE trips ALTER COLUMN username SET NOT NULL;
+    `);
+};
+
+exports.down = pgm => {
+    pgm.sql(`
+        ALTER TABLE trips ALTER COLUMN username DROP NOT NULL;
+        ALTER TABLE trips DROP COLUMN username;
+    `);
+};
+
+
