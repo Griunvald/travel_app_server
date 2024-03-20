ce836de885b22e418b436ecdd260eaf7dbc88839
Vitali Griunvald <me.griunvald@gmail.com>
2024-03-20 15:51:39 +0200

    Refactoring - change followed to following in repository function in order to avoid confusion with name of endpoint

--- server/repositories/FollowerRepository.js
+++ server/repositories/FollowerRepository.js
@@ -51,34 +51,30 @@ class FollowerRepository {
 
         }    
 
-async getFollowing(userId) {
-    const client = await this.pool.connect();
-    try {
-        // Select the leader_id (followed users' IDs) from the followers table
-        const selectQuery = `
-            SELECT usernames.user_id AS followed_user_id
-            FROM followers
-            JOIN usernames ON followers.leader_id = usernames.user_id
-            WHERE followers.follower_id = $1;
-        `;
+        async getFollowing(userId) {
+            const client = await this.pool.connect();
+            try {
+                const selectQuery = `
+                    SELECT usernames.user_id AS following_user_id
+                    FROM followers
+                    JOIN usernames ON followers.leader_id = usernames.user_id
+                    WHERE followers.follower_id = $1;
+                `;
 
-        const result = await client.query(selectQuery, [userId]);
-        
-        // Construct an object that includes both the requestor's userId
-        // and the array of IDs for the users they are following
-        const followingData = {
-            userId: userId, // The ID of the user making the request
-            followedUsersIds: result.rows.map(row => row.followed_user_id) // Map to get followed users' IDs
-        };
-
-        return followingData;
-    } catch(err) {
-        console.error(err);
-        throw err;
-    } finally {
-        client.release();
-    }
-}
+                const result = await client.query(selectQuery, [userId]);
+                
+                const followingData = {
+                    userId: userId,
+                    followingUsersIds: result.rows.map(row => row.following_user_id) 
+                };
+                return followingData;
+            } catch(err) {
+                console.error(err);
+                throw err;
+            } finally {
+                client.release();
+            }
+        }
 }
 
 export default new FollowerRepository(dbPool);
