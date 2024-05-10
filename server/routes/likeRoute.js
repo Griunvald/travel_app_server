3ff91c7c09c4270f625cf8c7879a60d307a59cad
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-10 18:43:10 +0300

    Add authGuart to a LikeController

--- server/routes/likeRoute.js
+++ server/routes/likeRoute.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import LikeController from '../controllers/LikeController.js';
 import LikeRepository from '../repositories/LikeRepository.js';
+import authGuard from '../middleware/authGuard.js';
 
 const router = Router();
 const likeController = new LikeController(LikeRepository);
 
-router.post('/:type/:id', likeController.addLike.bind(likeController));
-router.delete('/:type/:id', likeController.removeLike.bind(likeController));
-router.get('/user/:type/:id', likeController.getUserLikesCountByType.bind(likeController));
-router.get('/item/:type/:id', likeController.getItemLikesCountByType.bind(likeController));
+router.post('/:type/:id', authGuard, likeController.addLike.bind(likeController));
+router.delete('/:type/:id', authGuard, likeController.removeLike.bind(likeController));
+router.get('/user/:type/:id', authGuard, likeController.getUserLikesCountByType.bind(likeController));
+router.get('/item/:type/:id', authGuard, likeController.getItemLikesCountByType.bind(likeController));
 
 
 
