17c89b104fec1efeaca9bb197ef77f1328877e3f
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-14 08:31:02 +0300

    Add avatar key/value to user_info cookie on login

--- server/repositories/AuthRepository.js
+++ server/repositories/AuthRepository.js
@@ -2,117 +2,121 @@ import dbPool from '../db.js';
 import bcrypt from 'bcrypt';
 import { signJwt, verifyJwt } from '../utils/jwtUtils.js';
 import cookieParser from 'cookie-parser';
+import ProfileRepository from './ProfileRepository.js';
 
 class AuthRepository {
-    constructor(dbPool){
-        this.pool = dbPool;
-    }
+  constructor(dbPool) {
+    this.pool = dbPool;
+  }
 
-    async hashPassword(password) {
-        const saltRounds = 10;
-        return bcrypt.hash(password, saltRounds);
-    }
+  async hashPassword(password) {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+  }
 
-    async generateToken(id) {
-        const token = await signJwt({id});
-        return token;
-    }
+  async generateToken(id) {
+    const token = await signJwt({ id });
+    return token;
+  }
 
-    async createUser(email, fullname, username, password) {
-        const client = await this.pool.connect();
-        try {
-            await client.query(`BEGIN`);
+  async createUser(email, fullname, username, password) {
+    const client = await this.pool.connect();
+    try {
+      await client.query(`BEGIN`);
 
-            const usernameSelectQuery = `SELECT id FROM usernames WHERE username = $1`;
-            const usernameSelectResult = await client.query(usernameSelectQuery, [username]);
+      const usernameSelectQuery = `SELECT id FROM usernames WHERE username = $1`;
+      const usernameSelectResult = await client.query(usernameSelectQuery, [username]);
 
-            if(usernameSelectResult.rows.length > 0) {
-                throw new Error('Username already taken!');
-            }
+      if (usernameSelectResult.rows.length > 0) {
+        throw new Error('Username already taken!');
+      }
 
-            const hashedPassword = await this.hashPassword(password);
+      const hashedPassword = await this.hashPassword(password);
 
-            const userInsertQuery = `INSERT INTO users (email, fullname, password, username)
+      const userInsertQuery = `INSERT INTO users (email, fullname, password, username)
             VALUES ($1, $2, $3, $4)
             RETURNING id, email, username`;
-            
-            const values = [email, fullname, hashedPassword, username];
 
-            const userInsertResult = await  this.pool.query(userInsertQuery, values);
-            const userId = userInsertResult.rows[0].id;
-            const userInfo = JSON.stringify({username: userInsertResult.rows[0].username, userId });
+      const values = [email, fullname, hashedPassword, username];
 
-            const usernameInsertQuery = `INSERT INTO usernames (user_id, username) VALUES ($1, $2)`;
-            const usernameInsertResult  = await client.query(usernameInsertQuery, [userId, username]);
+      const userInsertResult = await this.pool.query(userInsertQuery, values);
+      const userId = userInsertResult.rows[0].id;
+      const userInfo = JSON.stringify({ username: userInsertResult.rows[0].username, userId });
 
-            await client.query('COMMIT')
+      const usernameInsertQuery = `INSERT INTO usernames (user_id, username) VALUES ($1, $2)`;
+      const usernameInsertResult = await client.query(usernameInsertQuery, [userId, username]);
 
-            const token = await this.generateToken(userId);
+      await client.query('COMMIT')
 
-            return {token, userInfo};
-            
-        } catch (err){
-            await client.query('ROLLBACK')
-            console.error(err);
-            throw err;
-        } finally {
-            client.release();
-        }
+      const token = await this.generateToken(userId);
+
+      return { token, userInfo };
+
+    } catch (err) {
+      await client.query('ROLLBACK')
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
-    async findUserByEmailOrUsername(input, string) {
-        const client = await this.pool.connect();
-        try {
-            const searchQuery = `SELECT * FROM users WHERE ${string} = $1 `;
-            const user = await client.query(searchQuery, [input] );
-            return user;
-        } catch(err) {
-            console.error(err);
-            throw err;
-        } finally {
-            client.release();
-        }
-    } 
-
-    async verifyPassword(user, password) {
-        if (!user || !user.rows || user.rows.length === 0) {
-            return false;
-        }
-        const hashedPassword = user.rows[0].password;
-        const match = await bcrypt.compare(password, hashedPassword)
-        return match;
+  async findUserByEmailOrUsername(input, string) {
+    const client = await this.pool.connect();
+    try {
+      const searchQuery = `SELECT * FROM users WHERE ${string} = $1 `;
+      const user = await client.query(searchQuery, [input]);
+      return user;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
-    async login(input, password) {
-        try {
-            let user;
-            if(input.includes('@')) {
-                user = await this.findUserByEmailOrUsername(input, 'email');
-            } else {
-                user = await this.findUserByEmailOrUsername(input, 'username');
-            }
-            if(user && await this.verifyPassword(user, password)) {
-                const token = await this.generateToken(user.rows[0].id);
-                const userInfo = JSON.stringify({
-                    username: user.rows[0].username,
-                    userId: user.rows[0].id});
-                return {token, userInfo};
-             } else {
-                 return null;
-             }
-        } catch(err){
-            throw err;
-        }
+  async verifyPassword(user, password) {
+    if (!user || !user.rows || user.rows.length === 0) {
+      return false;
     }
+    const hashedPassword = user.rows[0].password;
+    const match = await bcrypt.compare(password, hashedPassword)
+    return match;
+  }
 
-    async getUserIdFromToken(token){
-        const decodedToken = await verifyJwt(token);
-        const userId = decodedToken.userId;
-        return userId;
-        
+  async login(input, password) {
+    try {
+      let user;
+      if (input.includes('@')) {
+        user = await this.findUserByEmailOrUsername(input, 'email');
+      } else {
+        user = await this.findUserByEmailOrUsername(input, 'username');
+      }
+      if (user && await this.verifyPassword(user, password)) {
+        const token = await this.generateToken(user.rows[0].id);
+        const profile = await ProfileRepository.getProfile(user.rows[0].id);
+        const userInfo = JSON.stringify({
+          username: user.rows[0].username,
+          userId: user.rows[0].id,
+          avatar: profile.avatar || ''
+        });
+        return { token, userInfo };
+      } else {
+        return null;
+      }
+    } catch (err) {
+      throw err;
     }
+  }
 
-    
+  async getUserIdFromToken(token) {
+    const decodedToken = await verifyJwt(token);
+    const userId = decodedToken.userId;
+    return userId;
+
+  }
+
+
 };
 
 export default new AuthRepository(dbPool);
--- server/repositories/ProfileRepository.js
+++ server/repositories/ProfileRepository.js
@@ -2,44 +2,44 @@ import dbPool from '../db.js';
 import { toCamelCaseDeep } from '../utils/toCamelCase.js';
 
 class ProfileRepository {
-    constructor(dbPool){
-        this.pool = dbPool;
-    }
+  constructor(dbPool) {
+    this.pool = dbPool;
+  }
 
 
-    async  updateProfile(userId, profileData) {
-            const client = await this.pool.connect();
-        const { about, avatar, country, home_town, gender } = profileData;
+  async updateProfile(userId, profileData) {
+    const client = await this.pool.connect();
+    const { about, avatar, country, home_town, gender } = profileData;
 
-        const query = ` INSERT INTO profiles (user_id, about, avatar, country, home_town, gender) 
+    const query = ` INSERT INTO profiles (user_id, about, avatar, country, home_town, gender) 
         VALUES ($1, $2, $3, $4, $5, $6)
         ON CONFLICT (user_id) 
         DO UPDATE SET about = EXCLUDED.about, avatar = EXCLUDED.avatar, country = EXCLUDED.country,
             home_town = EXCLUDED.home_town, gender = EXCLUDED.gender;`
-            try {
-                return await client.query(query, [userId, about, avatar, country, home_town, gender]);
-            } catch (error) {
-                console.error('Error in updateProfile:', error);
-                throw error;
-            }
-        }
+    try {
+      return await client.query(query, [userId, about, avatar, country, home_town, gender]);
+    } catch (error) {
+      console.error('Error in updateProfile:', error);
+      throw error;
+    }
+  }
 
 
-    async  getProfile(userId) {
-            const client = await this.pool.connect();
+  async getProfile(userId) {
+    const client = await this.pool.connect();
 
-        const query = `SELECT about, avatar, country, home_town, gender FROM profiles WHERE user_id = $1`
-            try {
-                const result = await client.query(query, [userId]);
-                const profile = await result.rows[0] ? toCamelCaseDeep(result.rows[0]) : {};
-                return profile; 
-            } catch (error) {
-                console.error('Error in getProfile:', error);
-                throw error;
-            }
-        }
+    const query = `SELECT about, avatar, country, home_town, gender FROM profiles WHERE user_id = $1`
+    try {
+      const result = await client.query(query, [userId]);
+      const profile = await result.rows[0] ? toCamelCaseDeep(result.rows[0]) : {};
+      return profile;
+    } catch (error) {
+      console.error('Error in getProfile:', error);
+      throw error;
+    }
+  }
 
-    async getProfiles(limit, offset) {
+  async getProfiles(limit, offset) {
     const client = await this.pool.connect();
 
     const query = `
@@ -49,15 +49,15 @@ class ProfileRepository {
         LIMIT $1 OFFSET $2`;
 
     try {
-        const result = await client.query(query, [limit, offset]);
-        return result.rows.map(row => toCamelCaseDeep(row));
+      const result = await client.query(query, [limit, offset]);
+      return result.rows.map(row => toCamelCaseDeep(row));
     } catch (error) {
-        console.error('Error in getProfiles:', error);
-        throw error;
+      console.error('Error in getProfiles:', error);
+      throw error;
     } finally {
-        client.release();
+      client.release();
     }
-}
+  }
 
 };
 
