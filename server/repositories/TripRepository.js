2c88a9546a08e978ffd9335682bed096e20b1929
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-14 09:58:07 +0300

    Add avatar column to response of getAllTripsPreview

--- server/repositories/TripRepository.js
+++ server/repositories/TripRepository.js
@@ -3,107 +3,110 @@ import { signJwt } from '../utils/jwtUtils.js';
 import { toCamelCaseDeep } from '../utils/toCamelCase.js';
 
 class TripRepository {
-    constructor(dbPool){
-        this.pool = dbPool;
-    }
-    async createTrip(userId, username, title, description, url){
-        const tripStatus = await this.checkCurrentTripStatus(userId);
-        if (tripStatus === 'open') return 'open';
-        const client = await this.pool.connect();
-        const insertQuery = `INSERT INTO trips (user_id, title, description, url) VALUES ($1, $2, $3, $4) RETURNING id`;
-        const trip = await client.query(insertQuery, [userId, title, description, url]);
-        const tripId = JSON.stringify({tripId: trip.rows[0].id});
-        return tripId;
-    }
+  constructor(dbPool) {
+    this.pool = dbPool;
+  }
+  async createTrip(userId, username, title, description, url) {
+    const tripStatus = await this.checkCurrentTripStatus(userId);
+    if (tripStatus === 'open') return 'open';
+    const client = await this.pool.connect();
+    const insertQuery = `INSERT INTO trips (user_id, title, description, url) VALUES ($1, $2, $3, $4) RETURNING id`;
+    const trip = await client.query(insertQuery, [userId, title, description, url]);
+    const tripId = JSON.stringify({ tripId: trip.rows[0].id });
+    return tripId;
+  }
 
-    async checkCurrentTripStatus(userId){
-        const client = await this.pool.connect();
-        try{
-            const searchQuery = `SELECT status FROM trips WHERE user_id = $1 
+  async checkCurrentTripStatus(userId) {
+    const client = await this.pool.connect();
+    try {
+      const searchQuery = `SELECT status FROM trips WHERE user_id = $1 
             ORDER BY created_at DESC`;
-            const searchResult = await client.query(searchQuery, [userId]);
-               if (searchResult.rows.length === 0) {
-              return null;
-            }
-            const tripStatus =  searchResult.rows[0].status;
-            return tripStatus;
-            } catch(err){
-                console.error(err);
-                throw err;
-            } finally {
-                client.release();
-            }
-
+      const searchResult = await client.query(searchQuery, [userId]);
+      if (searchResult.rows.length === 0) {
+        return null;
+      }
+      const tripStatus = searchResult.rows[0].status;
+      return tripStatus;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
-    
-    async endCurrentTrip(userId){
-        const client = await this.pool.connect();
-        const tripStatus = await this.checkCurrentTripStatus(userId);
-            try{
-                if(tripStatus === 'open'){
-                    const updateQuery = `UPDATE trips SET status = 'closed' 
+
+  }
+
+  async endCurrentTrip(userId) {
+    const client = await this.pool.connect();
+    const tripStatus = await this.checkCurrentTripStatus(userId);
+    try {
+      if (tripStatus === 'open') {
+        const updateQuery = `UPDATE trips SET status = 'closed' 
                     WHERE user_id = $1 AND status = 'open'`;
-                    const updateResult = await client.query(updateQuery, [userId]);
-                } else if(tripStatus === 'closed'){
-                    return 'closed';
-                }
-            } catch(err){
-                console.error(err);
-                throw err;
-            } finally {
-                client.release();
-            }
+        const updateResult = await client.query(updateQuery, [userId]);
+      } else if (tripStatus === 'closed') {
+        return 'closed';
+      }
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
-    async getCurrentTrip(userId){
-        const client = await this.pool.connect();
-        try{
-            const searchQuery = `SELECT t.id, t.user_id, u.username, t.title, t.description, t.url, t.created_at 
+  async getCurrentTrip(userId) {
+    const client = await this.pool.connect();
+    try {
+      const searchQuery = `SELECT t.id, t.user_id, u.username, t.title, t.description, t.url, t.created_at 
             FROM trips AS t JOIN usernames AS u ON t.user_id = u.user_id WHERE t.user_id = $1 
             ORDER BY created_at DESC LIMIT 1`;
-            const searchResult = await client.query(searchQuery, [userId]);
-            if(searchResult.rows.length > 0){
-                const { id, title, username, description, url, created_at: createdAt } =  searchResult.rows[0];
-                return { id, username, title, description, url, createdAt };
-            }
-        } catch(err){
-            console.error(err);
-            throw err;
-        } finally {
-            client.release();
-        }
+      const searchResult = await client.query(searchQuery, [userId]);
+      if (searchResult.rows.length > 0) {
+        const { id, title, username, description, url, created_at: createdAt } = searchResult.rows[0];
+        return { id, username, title, description, url, createdAt };
+      }
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
-    async getAllTripsPreview(){
+  async getAllTripsPreview() {
 
-        const client = await this.pool.connect();
-        try{
-            const searchQuery = `SELECT trips.id, trips.user_id, usernames.username, trips.title, trips.description, trips.url, trips.created_at FROM trips
-            JOIN usernames ON trips.user_id = usernames.user_id ORDER BY created_at DESC`;
-            const searchResult = await client.query(searchQuery);
-            const results = [];
+    const client = await this.pool.connect();
+    try {
+      const searchQuery = `SELECT trips.id, trips.user_id, usernames.username, trips.title,
+            trips.description, trips.url, trips.created_at, profiles.avatar FROM trips
+            JOIN usernames ON trips.user_id = usernames.user_id
+            LEFT JOIN profiles ON trips.user_id = profiles.user_id 
+            ORDER BY created_at DESC`;
+      const searchResult = await client.query(searchQuery);
+      const results = [];
 
-            for (const row of searchResult.rows) {
-              const { id,  user_id: userId, username, title, description, url, created_at: createdAt } = row;
-              results.push({ id, userId, username, title, description, url, createdAt });
-            }
+      for (const row of searchResult.rows) {
+        const { id, user_id: userId, username, title, description, url, created_at: createdAt, avatar } = row;
+        results.push({ id, userId, username, title, description, url, createdAt, avatar });
+      }
 
-            return results;
-        } catch(err){
-            console.error(err);
-            throw err;
-        } finally {
-            client.release();
-        }
+      return results;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
 
 
-    async getCurrentTripRecordsWithTags(userId) {
-  const client = await this.pool.connect();
-  try {
-            const { id: tripId } = (await this.getCurrentTrip(userId)) ?? {};
-            const selectQuery = `
+  async getCurrentTripRecordsWithTags(userId) {
+    const client = await this.pool.connect();
+    try {
+      const { id: tripId } = (await this.getCurrentTrip(userId)) ?? {};
+      const selectQuery = `
               WITH RecordTags AS (
               SELECT rt.record_id, t.id AS tag_id, t.tag_name
               FROM record_tags rt
@@ -127,31 +130,31 @@ class TripRepository {
             ORDER BY r.order_number ASC;
             `;
 
-            const result = await client.query(selectQuery, [userId, tripId]);
-            
-            return toCamelCaseDeep(result);
-      } catch (err) {
-        console.error(err);
-        throw err;
-      } finally {
-        client.release();
-      }
+      const result = await client.query(selectQuery, [userId, tripId]);
+
+      return toCamelCaseDeep(result);
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
 
-    async getTripsCount(userId){
-        const client = await this.pool.connect();
-        try{
-            const searchQuery = `SELECT COUNT(*) as trips_count FROM trips WHERE user_id = $1;`
-            const searchResult = await client.query(searchQuery,[userId]);
-            return searchResult.rows[0].trips_count;
-        } catch(err){
-            console.error(err);
-            throw err;
-        } finally {
-            client.release();
-        }
+  async getTripsCount(userId) {
+    const client = await this.pool.connect();
+    try {
+      const searchQuery = `SELECT COUNT(*) as trips_count FROM trips WHERE user_id = $1;`
+      const searchResult = await client.query(searchQuery, [userId]);
+      return searchResult.rows[0].trips_count;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    } finally {
+      client.release();
     }
+  }
 
 }
 
