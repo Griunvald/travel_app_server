4f99ea27d55dc27adbdbee6ae07a1b42ab92e1cb
Vitali Griunvald <me.griunvald@gmail.com>
2024-05-18 19:08:47 +0300

    Refactor updateProfile function in ProfileRepository
    Change SQL query - update only specified values and not override existing

--- server/repositories/ProfileRepository.js
+++ server/repositories/ProfileRepository.js
@@ -9,20 +9,38 @@ class ProfileRepository {
 
   async updateProfile(userId, profileData) {
     const client = await this.pool.connect();
-    const { about, avatar, country, home_town, gender } = profileData;
-
-    const query = ` INSERT INTO profiles (user_id, about, avatar, country, home_town, gender) 
-        VALUES ($1, $2, $3, $4, $5, $6)
-        ON CONFLICT (user_id) 
-        DO UPDATE SET about = EXCLUDED.about, avatar = EXCLUDED.avatar, country = EXCLUDED.country,
-            home_town = EXCLUDED.home_town, gender = EXCLUDED.gender;`
     try {
-      return await client.query(query, [userId, about, avatar, country, home_town, gender]);
+      const updateFields = [];
+      const updateValues = [userId];
+
+      let paramIndex = 2;
+      for (const [key, value] of Object.entries(profileData)) {
+        if (value !== undefined) {
+          updateFields.push(`${key} = $${paramIndex}::text`);
+          updateValues.push(value);
+          paramIndex++;
+        }
+      }
+
+      if (updateFields.length === 0) {
+        throw new Error('No fields to update');
+      }
+
+      const query = `
+      UPDATE profiles
+      SET ${updateFields.join(', ')}
+      WHERE user_id = $1
+    `;
+
+      return await client.query(query, updateValues);
     } catch (error) {
       console.error('Error in updateProfile:', error);
       throw error;
+    } finally {
+      client.release();
     }
   }
+
 
 
   async getProfile(userId) {
