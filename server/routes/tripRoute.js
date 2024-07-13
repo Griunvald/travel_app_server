b449613a4f1392bb86f9038f3636052caa57a999
Vitali Griunvald <me.griunvald@gmail.com>
2024-07-13 23:13:22 +0300

    Fix bug - change routes order and rename

--- server/routes/tripRoute.js
+++ server/routes/tripRoute.js
@@ -11,9 +11,8 @@ router
   .post('/', authGuard, tripController.createTrip.bind(tripController))
   .patch('/close', authGuard, tripController.endCurrentTrip.bind(tripController))
   .get('/current', authGuard, tripController.getCurrentTrip.bind(tripController))
-  .get('/full-current/:userId', tripController.getFullCurrentTrip.bind(tripController))
-  .get('/full/:userId/:tripId', tripController.getFullTripByUserIdAndTripId.bind(tripController))
-  .get('/full/:userId', tripController.getFullCurrentTrip.bind(tripController)) // Catch-all current full trip for user
+  .get('/full/current/:userId', tripController.getFullCurrentTrip.bind(tripController)) // Specific route first
+  .get('/full/:userId/:tripId', tripController.getFullTripByUserIdAndTripId.bind(tripController)) // General route after
   .get('/preview/:limit/:offset', tripController.getAllTripsPreview.bind(tripController))
   .get('/count', authGuard, tripController.getTripsCount.bind(tripController))
   .get('/current/records/tags', tripController.getCurrentTripRecordsWithTags.bind(tripController))
